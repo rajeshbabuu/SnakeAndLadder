06b22b26ff91f837e1e7b0e05ddb4adaf6497613
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-25 21:50:47 +0530

    ensure the player gets to exact winning position 100

--- SnakeAndLadder/SnakeAndLadder/SnakeGame.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeGame.cs
@@ -8,16 +8,45 @@ namespace SnakeAndLadder
 {
     public class SnakeGame
     {
-        public void singlePlayer()
+        public const int FORWARD_MOVE = 1;
+        public const int BACKWARD_MOVE = 2;
+        public const int NO_MOVE = 3;
+
+        public void winningPosition()
         {
-            int num_Of_Player = 1;
-            int start_Position = 0;
-        }
-        public void firstDiceRoll()
-        {
-            Console.Write("First dice roll : ");
+            int position = 0;
+
             Random random = new Random();
-            Console.WriteLine(random.Next(1, 6));
+
+            while (position < 100)
+            {
+                int movingCheck = random.Next(1, 3);
+                int stepCheck = random.Next(1, 6);
+                Console.WriteLine(" this is moving check " + movingCheck);
+                Console.WriteLine(" this is step check " + stepCheck);
+                switch (movingCheck)
+                {
+                    case FORWARD_MOVE:
+                        if ((position + stepCheck) <= 100)
+                        {
+                            position = position + stepCheck;
+                            Console.WriteLine(" Forward move ");
+                        }
+                        break;
+                    case BACKWARD_MOVE:
+                        if (position > 0)
+                        {
+                            position = position - stepCheck;
+                            Console.WriteLine(" Backward move ");
+                        }
+                        break;
+                    case NO_MOVE:
+                        Console.WriteLine("No move ");
+                        break;
+                }
+                Console.WriteLine(position + " step count ");
+
+            }
         }
     }
 }
