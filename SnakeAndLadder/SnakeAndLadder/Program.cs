a34ec5c6e99aa79f244bfc42fa83000b772a86c6
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-25 21:15:25 +0530

    start position with single player

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Welcome to the Snake and Ladder Program------- ");
+
+            SnakeGame day4ProblemObj = new SnakeGame();
+
+            day4ProblemObj.singlePlayer();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeGame
+    {
+        public void singlePlayer()
+        {
+            int num_Of_Player = 1;
+            int start_Position = 0;
+
+            Console.WriteLine("Starting with only " + num_Of_Player + " player");
+            Console.WriteLine("First player position is : " + start_Position);
+        }
+    }
+}
